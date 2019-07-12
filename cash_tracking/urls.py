02b1da9ae2dab239e9484ca61f42005154814cb7
SirRwema <artumukunde@hotmail.com>
2019-07-12 18:07:28 +0300

    Added Auth Views

--- cash_tracking/urls.py
+++ cash_tracking/urls.py
@@ -1,11 +1,14 @@
+from django.urls import path
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic.base import TemplateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('^$', TemplateView.as_view(template_name='index.html'), name='home'),
-    url(r'^accounts/', include('django.contrib.auth.urls')), 
+    url(r'^login/$', auth_views.LoginView.as_view(template_name="registration/login.html"), name='login'), 
+    url(r'^logout/$', auth_views.LogoutView.as_view(template_name="index.html"), name='logout'),
     url(r'^blog_posts/', include('blog_posts.urls', namespace='blog_posts')),
     url(r'^agents/', include('agents.urls', namespace='agents')),
     url(r'^clients/', include('clients.urls', namespace='clients')),
